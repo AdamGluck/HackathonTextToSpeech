7e3cac6af3e7e6a379a768f6c096335536b769c2
AdamGluck <adamt.gluck@gmail.com>
2013-08-27 01:43:10 -0400

    added in a number of files

---
+++ HackathonTextToSpeech/DMTextToSpeechEasyAPI.h
@@ -0,0 +1,13 @@
+//
+//  DMTextToSpeechEasyAPI.h
+//  HackathonTextToSpeech
+//
+//  Created by Adam Gluck on 8/13/13.
+//  Copyright (c) 2013 DataMason. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DMTextToSpeechEasyAPI : NSObject
+
+@end
---
+++ HackathonTextToSpeech/DMTextToSpeechEasyAPI.m
@@ -0,0 +1,13 @@
+//
+//  DMTextToSpeechEasyAPI.m
+//  HackathonTextToSpeech
+//
+//  Created by Adam Gluck on 8/13/13.
+//  Copyright (c) 2013 DataMason. All rights reserved.
+//
+
+#import "DMTextToSpeechEasyAPI.h"
+
+@implementation DMTextToSpeechEasyAPI
+
+@end
