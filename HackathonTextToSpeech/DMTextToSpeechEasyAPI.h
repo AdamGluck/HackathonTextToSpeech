b334d12bc00d70d7828641a960382ae8cb44cb2f
AdamGluck <adamt.gluck@gmail.com>
2013-08-27 03:27:20 -0400

    documented

--- HackathonTextToSpeech/DMTextToSpeechEasyAPI.h
+++ HackathonTextToSpeech/DMTextToSpeechEasyAPI.h
@@ -23,33 +23,35 @@ typedef NS_ENUM(NSInteger, SimpleSpeechError){
     AudioError
 };
 @optional
-// these are called in response to attempts to authentication after an object is initialized... if it fails you should simply call retry with the object returned from initialization
+/* these are called in response to attempts to authentication after an object is initialized... if it fails you should simply call retry with the object returned from initialization */
 -(void)speechPreparationSucceeded;
 -(void)speechPreparationFailed;
 
-// this is called when text is not converted into speech... you might want to call -(void)readText:(NSSTring*)text and put the failed text string returned here into that method to attempt again
+/* this is called when text is not converted into speech... you might want to call -(void)readText:(NSSTring*)text and put the failed text string returned here into that method to attempt again */
 -(void)textConversionFailedFromText:(NSString *)text withError:(NSError *)error;
 
-// this is called when speech is recognized
+/* this is called when speech is recognized */
 -(void)speechWasRecognizedWithText:(NSString *)text;
 
-// these methods are called when speech is failed to be recognized...
-// if you just get "speech failed" you may want to use error.localizedDescription for more introspection
-// in particular with audio errors
+/* these methods are called when speech is failed to be recognized...
+ if you just get "speech failed" you may want to use error.localizedDescription for more introspection
+ in particular with audio errors */
 -(void)speechFailedWithSimpleSpeechError:(SimpleSpeechError)simpleError andNSError:(NSError *)error;
 
 @end
 
 @interface DMTextToSpeechEasyAPI : NSObject
 
-// simplest, just call this after the object is initialized and you'll hear your phone speak the text back to you!
+/* simplest, just call this after the object is initialized and you'll hear your phone speak the text back to you! */
 -(void)readText:(NSString *)text;
 
-// this exposes the ATTSpeechService object so you can call stop listening if need be
--(ATTSpeechService *)listen; 
-
-// a little more complicated
+/* two important delegate methods: recommended is implementing the delegate method speechPreparationSucceeded before allowing text to be read, also speechWasRecognizedWithText: needs to be implemented to get the response to listening */
 @property (weak, nonatomic) id <DMTextToSpeechEasyAPIDelegate> delegate;
--(void)retry; // call if speech preparation failed
+
+/* need to implement the method speechWasRecognizedWithText: in the delegate to get the speech returned from listen! */
+-(ATTSpeechService *)listen;
+
+/* this simply attempts to re-initialize the object */
+-(void)retry;
 
 @end
